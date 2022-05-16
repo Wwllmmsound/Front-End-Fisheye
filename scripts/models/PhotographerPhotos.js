d6f5a09ae17c1ab7cb22d4a2997f4175dd9f4b2b
WWLLMM <willemdav92@yahoo.fr>
2022-05-16 18:25:50 +0200

    This.name

--- scripts/models/PhotographerPhotos.js
+++ scripts/models/PhotographerPhotos.js
@@ -1,7 +1,8 @@
 import { PhotographerApi } from '../api/Api.js'
+import { PhotographerModel } from './PhotographerModel.js'
 
 class PhotographerPhotos {
-    constructor(photographerId, title, image, video, likes, date, price){
+    constructor(photographerId, title, image, video, likes, date, price, name){
         this._id = (new URL(document.location)).searchParams.get("id"),
         this._photographerId = photographerId,
         this._title = title,
@@ -10,8 +11,9 @@ class PhotographerPhotos {
         this._likes = likes,
         this._date = date,
         this._price = price,
-
-        this.photosApi = new PhotographerApi('../data/photographers.json')
+        this._name = name,
+        this.photosApi = new PhotographerApi('../data/photographers.json'),
+        this.photographersApi = new PhotographerApi('../data/photographers.json')
     }
     get id(){
         return this._id
@@ -25,11 +27,18 @@ class PhotographerPhotos {
     get title(){
         return this._title
     }
+    setImage(image){
+        this._image = image
+    }
+    setName(name){
+        this._name = name
+    }
     get image(){
-        return `../assets/photographers/${this._photographerId}/${this._image}`
+        console.log(this._name);
+        return `../assets/photographers/${this._name}/${this._image}`
     }
     get video(){
-        return `../assets/photographers/${this._photographerId}/${this._video}`
+        return `../assets/photographers/${this._name}/${this._video}`
     }
     get likes(){
         return this._likes
@@ -40,10 +49,11 @@ class PhotographerPhotos {
     get price(){
         return this._price
     }
+    
 
     // ________________________FUNCTION CREATING THE PHOTOS GRID______________________
 
-    async displayPhotos(media){
+    async displayPhotosCard(media){
         const photosList = this.PhotographerPhotoList(media);
         const photosSection = document.querySelector("#photosList");
 
@@ -64,9 +74,36 @@ class PhotographerPhotos {
         </figcaption>
     `;
         figure.innerHTML = photographerPhoto;
-
+        console.log(this._image);
+        console.log(this._name);
         return figure;
     }
+
+
+    async getPhotographerNameById(id){
+
+        let name = "";
+
+        const photographersHead = await this.photographersApi.getPhotographer();
+        const listPhotographers = photographersHead.map(photographer => new PhotographerModel(
+            photographer.name,
+            photographer.portrait,
+            photographer.city,
+            photographer.country,
+            photographer.tagline,
+            photographer.price,
+            photographer.id
+        ))
+        console.log(listPhotographers);
+
+        for (let photographer of listPhotographers) {
+            if (photographer.id == id) {
+                name = photographer.name;
+            }
+        }
+
+        return name;
+}
 
     async init(){
         const allPhotos = await this.photosApi.getMedia();
@@ -82,12 +119,20 @@ class PhotographerPhotos {
 
         for (let media of listPhotos) {
             if (media.photographerId == this._id) {
-                this.displayPhotos(media);
-                console.log("Photo Card")
+                let name = await this.getPhotographerNameById(media.photographerId);
+                this.setName(name);
+                this.setImage(media.image);
+                media.name = this._name;
+                this.displayPhotosCard(media);
+                console.log(name);
             }
         }
     }
+
+
+    
 }
+
 
 const photosDisplayed = new PhotographerPhotos();
 photosDisplayed.init();
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -18,3 +18,4 @@ function sendMessage(e){
     e.preventDefault();
     closeModal();
 }
+
